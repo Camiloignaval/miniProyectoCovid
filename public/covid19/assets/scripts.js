a08d8da40f8413fe2b7660317d6837a38f9be54d
Camiloignaval <camiloignaval@gmail.com>
2021-06-09 19:37:33 -0400

    antes de empezar a modificar

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -6,26 +6,94 @@ const getTotal = async (addLink) => {
     return data
 }
 
-// const grafico = (data) => {
-//     let ctx = document.getElementById('grafico')
-//     let barChart = new Chart(ctx, {
-//         type: 'bar',
-//         data: {
-//             labels: data.map((el) => el.location),
-//             datasets: [{
-//                 label: 'life expectancy (years)',
-//                 backgroundColor: 'rgba(128,164,237, 0.8)',
-//                 borderColor: 'rgba(128,164,237, 1)',
-//                 data: dataset.map((el) => el.confirmed)
-//             }]
-//         }
-//     })
-//     ctx.innerHTML = barChart
-// }
+const rellenarGrafico = (datos, id) => {
+    let nombres = datos.map(pais => pais.location)
+
+    let activos = datos.map(pais => pais.confirmed)
+    let confirmados = datos.map(pais => pais.deaths)
+    let muertos = datos.map(pais => pais.recovered)
+    let recuperados = datos.map(pais => pais.active)
+
+    new Chart(document.getElementById(id), {
+        type: 'bar',
+        data: {
+            labels: nombres,
+            datasets: [
+                {
+                    label: "Casos activos",
+                    backgroundColor: "#CB2E2E",
+                    data: activos
+                }, {
+                    label: "casos confirmados",
+                    backgroundColor: "#F0D522",
+                    data: confirmados
+                }, {
+                    label: "casos muertos",
+                    backgroundColor: "#939393",
+                    data: muertos
+                }, {
+                    label: "casos recuperados",
+                    backgroundColor: "#22A5F0",
+                    data: recuperados
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Population growth (millions)'
+            }
+        }
+    });
+
+}
+
+const rellenoModal = async (pais) => {
+    let nombreJunto = pais.split(' ').join('');
+    // console.log(nombreJunto);
+    let datos = await getTotal(`countries/${nombreJunto}`);
+    console.log(datos)
+    // rellenarGrafico(datos, `graficoModal${datos.location}`)
+    // FORMA SIN FUNCIONES
+    new Chart(document.getElementById(`graficoModal${datos.Location}`), {
+        type: 'bar',
+        data: {
+            labels: datos.Location,
+            datasets: [
+                {
+                    label: "Casos activos",
+                    backgroundColor: "#CB2E2E",
+                    data: datos.active
+                }, {
+                    label: "casos confirmados",
+                    backgroundColor: "#F0D522",
+                    data: datos.confirmed
+                }, {
+                    label: "casos muertos",
+                    backgroundColor: "#939393",
+                    data: datos.deaths
+                }, {
+                    label: "casos recuperados",
+                    backgroundColor: "#22A5F0",
+                    data: datos.recovered
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Population growth (millions)'
+            }
+        }
+    });
+
+}
+
 
 const imprimirTabla = (array) => {
     let tabla = document.getElementById('cuerpoTabla');
     let relleno = array.map(dato => {
+
         return ` <tr>
     <th>${dato.location}</th>
     <td>${dato.confirmed}</td>
@@ -44,7 +112,7 @@ const imprimirTabla = (array) => {
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>${dato.active}</p>
+                    <canvas id="graficoModal${dato.location}"  width="400" height="400"></canvas>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -52,14 +120,12 @@ const imprimirTabla = (array) => {
             </div>
         </div>
     </div>
-` }).join('');
+`
+    }).join('');
 
     tabla.innerHTML = relleno;
 }
 
-const rellenoModal = (pais) => {
-
-}
 
 btnsVer = document.querySelectorAll('.verMas')
 btnsVer.forEach(btn => {
@@ -71,9 +137,13 @@ btnsVer.forEach(btn => {
 let funcionPrincipal = async () => {
     let datos = await getTotal('total')
     let datosFiltrados = datos.filter(dato => dato.active > 10000)
+    // let dato = await getTotal('countries/SolomonIslands')
+    // console.log(dato)
+
     console.log(datosFiltrados)
-    // grafico(datosFiltrados)
+    rellenarGrafico(datosFiltrados, "graficoPrincipal")
     imprimirTabla(datos)
+    rellenoModal('India')
 
 }
 
